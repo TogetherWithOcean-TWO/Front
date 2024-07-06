75d1f8a69c3a4891b8eb40bd4dbde799671a8dc5
YU JEONG SEON <yjs000616@gmail.com>
2024-07-06 17:20:28 +0900

    SCRUM-12 <message>

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import SplashScreen from "./screens/SplashScreen/SplashScreen";
-import LoginScreen from "./screens/loginScreen/LoginScreen";
-import SignupScreen from "./screens/signupScreen/SignupScreen";
-import FindForLoginScreen from "./screens/loginScreen/FindForLoginScreen";
+import SplashScreen from "./src/screens/SplashScreen/SplashScreen";
+import LoginScreen from "./src/screens/loginScreen/LoginScreen";
+import SignupScreen from "./src/screens/signupScreen/SignupScreen";
+import FindForLoginScreen from "./src/screens/loginScreen/FindForLoginScreen";
 
 const Stack = createStackNavigator();
 
@@ -17,13 +17,19 @@ const App = () => {
           //options={{ headerShown: false }}
           options={{ title: "overView" }}
         /> */}
-        {/* <Stack.Screen name="Login" component={LoginScreen} /> */}
         {/* <Stack.Screen name="Signup" component={SignupScreen} /> */}
-        <Stack.Screen
+
+        {/* 아이디/비밀번호 찾기 */}
+        {/*<Stack.Screen
           name="Findforlogin"
           component={FindForLoginScreen}
           options={{ headerShown: false }}
-        />
+        />*/}
+
+        {/*splash 화면*/}
+        <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} /> 
+        {/*<Stack.Screen name="Login" component={LoginScreen} />*/}
+        
       </Stack.Navigator>
     </NavigationContainer>
   );
