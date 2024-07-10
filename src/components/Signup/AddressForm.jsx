1b27b578a8152e4ee4c4a7160e0139c9d2d8cc82
sujin209 <20201036@sungshin.ac.kr>
2024-07-10 13:54:58 +0900

    SCRUM-54

---
+++ src/components/Signup/AddressForm.jsx
@@ -0,0 +1,66 @@
+import { View, TouchableOpacity } from "react-native";
+import { LabelTitle } from "../common/CustomText";
+import { CustomInput } from "../common/CustomInput";
+import EStyleSheet from "../../styles/global";
+import { useState } from "react";
+import Icon from "react-native-vector-icons/Foundation";
+
+export const AddressForm = () => {
+  const [basicAddress, setBasicAddress] = useState("");
+  const [detailedAddress, setDetailedAddress] = useState("");
+  const [postalNumber, setPostalNumber] = useState("");
+  return (
+    <View>
+      <View style={styles.label}>
+        <LabelTitle text="주소" />
+      </View>
+      <View style={[styles.inputWithButton, styles.formGroup]}>
+        <CustomInput
+          placeholder="우편번호"
+          value={postalNumber}
+          onChangeText={(text) => setPostalNumber(text)}
+        />
+        <TouchableOpacity
+          style={styles.searchButton}
+          onPress={() => {
+            /* 검색 */
+          }}
+        >
+          <Icon name="magnifying-glass" size={22} color="#A8A8A8" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.formGroup}>
+        <CustomInput
+          placeholder="기본 주소"
+          value={basicAddress}
+          onChangeText={(text) => setBasicAddress(text)}
+        />
+      </View>
+      <View style={styles.formGroup}>
+        <CustomInput
+          placeholder="상세 주소를 입력해주세요"
+          value={detailedAddress}
+          onChangeText={(text) => setDetailedAddress(text)}
+        />
+      </View>
+      <LabelTitle text="쓰레기 봉투를 배급 받으실 주소를 입력해주세요 :)" />
+    </View>
+  );
+};
+
+const styles = EStyleSheet.create({
+  label: {
+    marginTop: 10,
+  },
+  formGroup: {
+    marginVertical: 8,
+  },
+  inputWithButton: {
+    position: "relative",
+  },
+  searchButton: {
+    position: "absolute",
+    right: 5,
+    padding: 10,
+  },
+});
---
+++ src/components/common/CustomText.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import { Text } from "react-native";
+import EStyleSheet from "../../styles/global";
+
+// MainTitle
+export const MainTitle = ({ text }) => {
+  return <Text style={[styles.text, styles.mainText]}>{text}</Text>;
+};
+
+// LabelTitle
+export const LabelTitle = ({ text }) => {
+  return <Text style={[styles.text, styles.labelText]}>{text}</Text>;
+};
+
+// SubTitle
+export const SubTitle = ({ text }) => {
+  return <Text style={[styles.text, styles.subTitle]}>{text}</Text>;
+};
+
+// ErrorText
+export const ErrorText = ({ text }) => {
+  return <Text style={[styles.text, styles.errorText]}>{text}</Text>;
+};
+
+// InfoText
+export const InfoText = ({ text }) => {
+  return <Text style={[styles.text, styles.InfoText]}>{text}</Text>;
+};
+
+const styles = EStyleSheet.create({
+  text: {
+    color: "$Blue01",
+    fontWeight: "bold",
+    fontFamily: "Pretendard",
+  },
+  mainText: {
+    fontSize: 30,
+  },
+  subTitle: {
+    fontSize: 20,
+  },
+  labelText: {
+    fontSize: 16,
+    marginVertical: 5,
+    marginLeft: 5,
+  },
+  errorText: {
+    color: "$Red01",
+    marginHorizontal: 10,
+    marginTop: 5,
+    fontSize: 12,
+  },
+  InfoText : {
+    color : "$Blue01",
+    fontSize : 16,
+    fontWeight : "bold",
+    textAlign : "center"
+  }
+});
