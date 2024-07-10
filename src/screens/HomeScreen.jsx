40def03488a54b3c98c181117c2e90ac4ffdd30f
YU JEONG SEON <yjs000616@gmail.com>
2024-07-10 11:56:02 +0900

    update

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -9,13 +9,6 @@ const HomeScreen = () => {
     return (
       <View style={styles.container}>
         <LogoBar/>
-        <LinearGradient
-            colors={['#ff9a9e', '#fad0c4']} // 원하는 색상 배열로 변경
-            start={{ x: 0, y: 0 }} // 그라데이션 시작점 (위쪽)
-            end={{ x: 0, y: 1 }}   // 그라데이션 끝점 (아래쪽)
-            style={styles.gradient}
-        >
-        </LinearGradient>
         <View style={styles.greetingContainer}>
           <SubTitle text="안녕하세요, abc님!" />
           <SubTitle text="제주 곽지 해수욕장에 가보는건 어떨까요?" />
