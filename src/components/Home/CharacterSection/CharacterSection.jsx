023112d127c66cdfc15291b37c7f148b4a64a908
YU JEONG SEON <yjs000616@gmail.com>
2024-07-14 20:37:08 +0900

    홈 화면 캡처 기능

--- src/components/Home/CharacterSection/CharacterSection.jsx
+++ src/components/Home/CharacterSection/CharacterSection.jsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from 'react';
-import { View, Text,StyleSheet } from 'react-native';
+import { View,StyleSheet, Image, Text } from 'react-native';
 import Character from './Character';
 import CharacterButton from './CharacterButton';
 import { useUserInfo } from '../../../contexts/UserInfoContext';
 import { useNavigation } from '@react-navigation/native';
+import ViewShot from 'react-native-view-shot';
+import { requestPermissionsAsync, createAssetAsync } from 'expo-media-library';
 import { captureView } from '../utils/capture';
-
 
 
 import seal from '../../../assets/images/charactor/seal.png';
@@ -18,9 +19,9 @@ import { LabelTitle } from '../../common/CustomText';
 const CharacterSection = () => {
   const { userInfo } = useUserInfo();
   const navigation = useNavigation();
-  const viewRef = useRef(null);
-  const [capturedImage, setCapturedImage] = useState(null);
 
+  const viewRef = useRef();
+  const [capturedUri, setCapturedUri] = useState(null);
 
   const getCharacterImage = (charId) => {
     switch (charId) {
@@ -41,12 +42,21 @@ const CharacterSection = () => {
 
   const handleCapture = async () => {
     try {
-      const uri = await captureView(viewRef.current);
-      setCapturedImage(uri);
+      const { status } = await requestPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Permission to access media library is required!');
+        return;
+      }
+
+      const uri = await viewRef.current.capture();
+      await createAssetAsync(uri);
+      
+      setCapturedUri(null);  //캡처 후 uri 상태 초기화
     } catch (error) {
-      console.error('Error capturing view: ', error);
+      console.error('Capture failed', error);
     }
   };
+
 
   if (characterImage === null) {
     return null; // 캐릭터 이미지가 설정되지 않은 경우 렌더링하지 않음
@@ -55,25 +65,22 @@ const CharacterSection = () => {
   return (
     <View style={styles.container}>
       <View style={styles.sideButtons}>
-        <CharacterButton iconName="camera-outline" label="캡쳐" onPress={handleCapture} />
+        <CharacterButton iconName="camera-outline" label="캡쳐"  onPress={handleCapture} />
         <CharacterButton iconName="color-wand-outline" label="꾸미기" onPress={() => navigation.navigate("CharactorCustomScreen")} />
         <CharacterButton iconName="refresh-outline" label="캐릭터 변경" onPress={() => navigation.navigate('SignupCharacter', {fromHome : true})} />
       </View>
-      <View style={styles.characterContainer}>
+    
+      <ViewShot ref={viewRef} style={styles.characterContainer}>
         <Character imageSource={characterImage} />
-        <LabelTitle text={userInfo.charName} style={styles.characterName}/>
-      </View>
+        <LabelTitle text={userInfo.charName} style={styles.characterName} />
+      </ViewShot>
+      
+      
       <View style={styles.sideButtons}>
         <CharacterButton iconName="cart-outline" label="상점" onPress={() => navigation.navigate('StoreScreen')} />
         <CharacterButton iconName="fish-outline" label="도감" onPress={() => navigation.navigate('MarinBookScreen')} />
         <CharacterButton iconName="map-outline" label="나의 해양" onPress={() => navigation.navigate('MyOcean')} />
       </View>
-      {capturedImage && (
-        <Image
-          source={{ uri: capturedImage }}
-          style={styles.capturedImage}
-        />
-      )}
     </View>
   );
 };
@@ -85,8 +92,8 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    //borderWidth: 2, // 테두리 두께
-    //borderColor: 'black', // 테두리 색상
+    borderWidth: 2, // 테두리 두께
+    borderColor: 'black', // 테두리 색상
     
   },
   sideButtons: {
