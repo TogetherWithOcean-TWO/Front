105aff57842d9627b8b07ca364438eceacb33930
hidanbeing <nd6162bv@gmail.com>
2024-07-16 23:26:07 +0900

    ✨ SCRUM-96 설정스크린 : 페이지 구조 잡기 완료 ..

---
+++ src/components/Setting/EditProfileForm.jsx
@@ -0,0 +1,27 @@
+import { React, useState } from "react";
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import { MainTitle, SubTitle, LabelTitle } from "../common/CustomText";
+import EStyleSheet from "../../styles/global";
+import { SignupForm } from "../Signup/SignupForm";
+import { WideButton } from "../common/CustomButton";
+export const EditProfileForm = () => {
+  const [isValid, setIsValid] = useState(false);
+  return (
+    <View style={styles.container}>
+      <MainTitle text="개인정보 수정" />
+      <SignupForm setIsValid={setIsValid} />
+      <View style={styles.button}>
+        <WideButton text="확인" disabled={!isValid} />
+      </View>
+    </View>
+  );
+};
+
+const styles = EStyleSheet.create({
+  container: {
+    padding: 25,
+  },
+  button: {
+    marginTop: 20,
+  },
+});
