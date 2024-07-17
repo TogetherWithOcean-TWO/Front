357323f34078dc489d5821076320c29ca9e784c7
YU JEONG SEON <yjs000616@gmail.com>
2024-07-18 01:35:54 +0900

    app.json에서 민감한 정보를 제거하고 App.config.js 사용

---
+++ apikey.js
@@ -0,0 +1,8 @@
+
+const EXPO_PROJECT_ID = '9ec23bde-94e2-448b-9ca3-ad6171e395c3';
+const GOOGLE_MAPS_API_KEY = 'AIzaSyBW_54AgleHcRFHWVf4bEmxDkgoJEwNLYg';
+
+module.exports = {
+    EXPO_PROJECT_ID,
+    GOOGLE_MAPS_API_KEY,
+  };
